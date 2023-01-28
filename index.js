5d9f19fe05e7142fa8b2fa7f08ce4b41324f3f79
Satyaki Chatterjee <satyaki_93@yahoo.in>
2023-01-28 15:20:45 +0530

    fix: vercel config

--- index.js
+++ index.js
@@ -4,32 +4,36 @@ const bodyParser = require("body-parser");
 const axios = require("axios");
 
 const { TOKEN, SERVER_URL, PORT } = process.env;
-const TELEGRAM_API=`https://api.telegram.org/bot${TOKEN}`;
-const URI=`/webhook/${TOKEN}`;
-const WEBHOOK_URL=`${SERVER_URL}${URI}`;
+const TELEGRAM_API = `https://api.telegram.org/bot${TOKEN}`;
+const URI = `/webhook/${TOKEN}`;
+const WEBHOOK_URL = `${SERVER_URL}${URI}`;
 
 const app = express();
 app.use(bodyParser.json());
 
 const init = async () => {
-    const webhookUrl = `${TELEGRAM_API}/setWebhook?url=${WEBHOOK_URL}`;
-    const res = await axios.get(`${webhookUrl}`);
-    console.log("init", res.data);
-}
+  console.log(WEBHOOK_URL);
 
-app.post(URI, async(req, res) => {
-    console.log(req);
-    
-    const chatId = req.body.message.chat.id;
-    const text = req.body.message.text;
+  const webhookUrl = `${TELEGRAM_API}/setWebhook?url=${WEBHOOK_URL}`;
+  const res = await axios.get(`${webhookUrl}`);
+  console.log("init", res.data);
+};
 
-    await axios.post(`${TELEGRAM_API}/sendMessage`, {
-        chat_id: chatId,
-        text: text
-    })
-})
+app.post(URI, async (req, res) => {
+  console.log(req);
 
-app.listen(PORT || 5000, async()=> {
-    console.log(`App running on ${PORT || 5000}`);
-    await init()
+  const chatId = req.body.message.chat.id;
+  const text = req.body.message.text;
+
+  await axios.post(`${TELEGRAM_API}/sendMessage`, {
+    chat_id: chatId,
+    text: text,
+  });
 });
+
+app.listen(PORT || 5000, async () => {
+  console.log(`App running on ${PORT || 5000}`);
+  await init();
+});
+
+module.exports = app;
